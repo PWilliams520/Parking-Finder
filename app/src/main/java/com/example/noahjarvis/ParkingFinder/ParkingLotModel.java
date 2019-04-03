fbc0401eda844fb8f0db345c0b02fee7bae75659
Esteban Murillo <stbamb@gmail.com>
2019-04-03 18:18:34 -0400

    Adding code to retrieve data from Firebase

---
+++ app/src/main/java/com/example/noahjarvis/ParkingFinder/ParkingLotModel.java
@@ -0,0 +1,50 @@
+package com.example.noahjarvis.ParkingFinder;
+
+public class ParkingLotModel {
+
+    private String A;
+    private String B;
+    private String C;
+    private String D;
+    private String E;
+
+    public String getA() {
+        return A;
+    }
+
+    public void setA(String a) {
+        A = a;
+    }
+
+    public String getB() {
+        return B;
+    }
+
+    public void setB(String b) {
+        B = b;
+    }
+
+    public String getC() {
+        return C;
+    }
+
+    public void setC(String c) {
+        C = c;
+    }
+
+    public String getD() {
+        return D;
+    }
+
+    public void setD(String d) {
+        D = d;
+    }
+
+    public String getE() {
+        return E;
+    }
+
+    public void setE(String e) {
+        E = e;
+    }
+}
