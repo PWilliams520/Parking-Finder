da37eb314d7bbb03a291c4b69de27a148113dab0
noahj13 <noahjarvis0@gmail.com>
2019-04-03 22:52:52 -0400

    Maps API partially implemented

--- app/src/main/java/com/example/noahjarvis/ParkingFinder/DetailsActivity.java
+++ app/src/main/java/com/example/noahjarvis/ParkingFinder/DetailsActivity.java
@@ -1,25 +1,46 @@
 package com.example.noahjarvis.ParkingFinder;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class DetailsActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class DetailsActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
         ParkingLot lot = (ParkingLot) getIntent().getSerializableExtra("Current Lot");
         setTitle(lot.getName());
-        LinearLayout layout= new LinearLayout(this);
-        setContentView(layout);
-        TextView test = new TextView(this);
-        test.setText(lot.toString());
-        layout.addView(test);
+        //TextView test = findViewById(R.layout.map_title);
+        //test.setText(lot.toString());
 
 
+
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
